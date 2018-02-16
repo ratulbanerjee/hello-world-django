632925e6e1f5e4d41c5606c6247b547288fc93fe
subramony <subbufrom@gmail.com>
2018-02-16 15:59:50 +0530

    5th commit

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chown ubuntu:ubuntu /home/ec2-user/python_project
+chown ubuntu:ubuntu /home/ubuntu/python_project
 virtualenv /home/ubuntu/python_project/venv
 chown ubuntu:ubuntu /home/ubuntu/python_project/venv
 chown ubuntu:ubuntu /home/ubuntu/python_project/venv/*
