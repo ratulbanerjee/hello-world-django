72f1ba8158546d9ff3c2b1ef1d01654bca0470e8
subramony <subbufrom@gmail.com>
2018-02-16 15:08:41 +0530

    3rd commit

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,5 +3,5 @@
 CURRENT_IP=`ifconfig | grep inet | head -1 | awk '{ print $2 }' | awk -F':' '{ print $2 }'`
 cd /home/ubuntu/python_project
 source /home/ubuntu/python_project/venv/bin/activate
-nohup python /home/ubuntu/python_project/manage.py runserver ${CURRENT_IP}:8000 &
+nohup python /home/ubuntu/python_project/manage.py runserver ${CURRENT_IP}:8000 >> /dev/null > man.log &
 
