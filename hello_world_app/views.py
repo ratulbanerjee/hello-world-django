305743ff590a92f10c9da1ad83f10bff7a068e30
subramony <subbufrom@gmail.com>
2018-02-17 12:13:34 +0530

    8th commit

--- hello_world_app/views.py
+++ hello_world_app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('welcome ratulb')
+    return HttpResponse('jjgsjgjjgvjobjfdojopbjpod jjrpahopjsgjopjo jopjgb djobod[okz')
